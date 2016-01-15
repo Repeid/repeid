ae0922338cd0d21c7b5861ef693f11f63ae87ab1
carlos <carlosthe19916@gmail.com>
2016-01-15 08:41:49 -0500

    test

--- manager/test/api/model/src/test/java/org/repeid/manager/test/api/model/AbstractTest.java
+++ manager/test/api/model/src/test/java/org/repeid/manager/test/api/model/AbstractTest.java
@@ -5,16 +5,26 @@ import java.io.File;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.persistence.UsingDataSet;
+
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.descriptor.api.Descriptors;
+import org.jboss.shrinkwrap.descriptor.api.beans10.BeansDescriptor;
 import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+
 import org.junit.runner.RunWith;
 import org.repeid.manager.api.beans.exceptions.StorageException;
 import org.repeid.manager.api.beans.representations.security.PermissionType;
 import org.repeid.manager.api.jpa.AbstractJpaStorage;
+import org.repeid.manager.api.jpa.DefaultJpaConnectionProvider;
 import org.repeid.manager.api.jpa.entities.security.UserEntity;
+import org.repeid.manager.api.jpa.models.JpaAccionistaProvider;
+import org.repeid.manager.api.jpa.models.JpaPersonaJuridicaProvider;
+import org.repeid.manager.api.jpa.models.JpaPersonaNaturalProvider;
 import org.repeid.manager.api.jpa.models.JpaTipoDocumentoProvider;
+import org.repeid.manager.api.jpa.models.security.JpaRoleProvider;
 import org.repeid.manager.api.jpa.models.security.JpaUserProvider;
 import org.repeid.manager.api.model.Model;
 import org.repeid.manager.api.model.enums.TipoPersona;
@@ -38,6 +48,8 @@ public abstract class AbstractTest {
         File[] dependencies = Maven.resolver().resolve("org.slf4j:slf4j-simple:1.7.10").withoutTransitivity()
                 .asFile();
 
+        BeansDescriptor beansXml = Descriptors.create(BeansDescriptor.class);
+        
         WebArchive war = ShrinkWrap.create(WebArchive.class, "test.war")
                 //.addPackage(WarApiManagerConfig.class.getPackage())
                 .addPackage(StorageException.class.getPackage())
@@ -68,10 +80,24 @@ public abstract class AbstractTest {
 
                 .addAsResource("META-INF/test-persistence.xml", "META-INF/persistence.xml")
                 .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml")
-                .addAsWebInfResource("test-ds.xml");
+                .addAsWebInfResource("test-ds.xml")
+                
+                .addAsManifestResource(
+                        new StringAsset(beansXml.getOrCreateAlternatives()
+                                .clazz(DefaultJpaConnectionProvider.class.getName())
+                                
+                                .clazz(JpaTipoDocumentoProvider.class.getName())
+                                .clazz(JpaPersonaNaturalProvider.class.getName())
+                                .clazz(JpaPersonaJuridicaProvider.class.getName())
+                                .clazz(JpaAccionistaProvider.class.getName())
+                                .clazz(JpaUserProvider.class.getName())
+                                .clazz(JpaRoleProvider.class.getName())
+                                .up().exportAsString()),
+                        beansXml.getDescriptorName());
 
         war.addAsLibraries(dependencies);
 
         return war;
-    }
+    }    
+    
 }
