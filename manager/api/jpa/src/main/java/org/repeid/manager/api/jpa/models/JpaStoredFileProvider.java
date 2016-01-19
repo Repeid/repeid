28e1e2ed363514c1b1cc3f243b7b49487dd5200e
carlos <carlosthe19916@gmail.com>
2016-01-19 10:01:49 -0500

    jpa stored file not import

--- manager/api/jpa/src/main/java/org/repeid/manager/api/jpa/models/JpaStoredFileProvider.java
+++ manager/api/jpa/src/main/java/org/repeid/manager/api/jpa/models/JpaStoredFileProvider.java
@@ -17,15 +17,11 @@
  *******************************************************************************/
 package org.repeid.manager.api.jpa.models;
 
-import java.util.Locale;
 import java.util.UUID;
 
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
-import javax.inject.Inject;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 
 import org.repeid.manager.api.jpa.AbstractJpaStorage;
 import org.repeid.manager.api.jpa.entities.FileEntity;
@@ -34,15 +30,9 @@ import org.repeid.manager.api.jpa.entities.StoredFileEntity;
 import org.repeid.manager.api.model.StoreConfigurationModel;
 import org.repeid.manager.api.model.StoredFileModel;
 import org.repeid.manager.api.model.StoredFileProvider;
-import org.repeid.manager.api.model.box.DropboxProvider;
-import org.repeid.manager.api.model.box.GoogleDriveProvider;
 import org.repeid.manager.api.model.enums.StoreConfigurationType;
 import org.repeid.manager.api.model.provider.ProviderFactory;
 import org.repeid.manager.api.model.provider.ProviderType;
-
-import com.dropbox.core.DbxClient;
-import com.dropbox.core.DbxEntry;
-import com.dropbox.core.DbxRequestConfig;
 
 /**
  * @author <a href="mailto:carlosthe19916@sistcoop.com">Carlos Feria</a>
@@ -52,12 +42,6 @@ import com.dropbox.core.DbxRequestConfig;
 @TransactionAttribute(TransactionAttributeType.REQUIRED)
 @ProviderFactory(ProviderType.JPA)
 public class JpaStoredFileProvider extends AbstractJpaStorage implements StoredFileProvider {
-
-	@Inject
-	private DropboxProvider dropboxProvider;
-
-	@Inject
-	private GoogleDriveProvider googleDriveProvider;
 
 	@Override
 	public void close() {
@@ -76,10 +60,6 @@ public class JpaStoredFileProvider extends AbstractJpaStorage implements StoredF
 		switch (provider) {
 		case localhost:
 			return createLocalFile(file, configuration);
-		case Dropbox:
-			return createDropboxFile(file, configuration);
-		case GoogleDrive:
-			return createGoogleDriveFile(file, configuration);
 		default:
 			return null;
 		}
@@ -103,30 +83,6 @@ public class JpaStoredFileProvider extends AbstractJpaStorage implements StoredF
 
 		getEntityManager().persist(storedFileEntity);
 		return new StoredFileAdapter(getEntityManager(), storedFileEntity);
-	}
-
-	private StoredFileModel createDropboxFile(byte[] file, StoreConfigurationModel configuration) {
-		// File storage
-		DbxRequestConfig config = new DbxRequestConfig(configuration.getAppKey(), Locale.getDefault().toString());
-		DbxClient dbxClient = new DbxClient(config, configuration.getToken());
-		DbxEntry.File fileEntity = dropboxProvider.upload(dbxClient, file);
-
-		// Store configuration entity
-		StoreConfigurationEntity storeConfigurationEntity = StoreConfigurationAdapter
-				.toStoreConfigurationEntity(configuration, getEntityManager());
-
-		// Create StoreFileEntity
-		StoredFileEntity storedFileEntity = new StoredFileEntity();
-		storedFileEntity.setFileId(fileEntity.name);
-		storedFileEntity.setUrl(fileEntity.path);
-		storedFileEntity.setStoreConfiguration(storeConfigurationEntity);
-
-		getEntityManager().persist(storedFileEntity);
-		return new StoredFileAdapter(getEntityManager(), storedFileEntity);
-	}
-
-	private StoredFileModel createGoogleDriveFile(byte[] file, StoreConfigurationModel configuration) {
-		return null;
 	}
 
 	@Override
