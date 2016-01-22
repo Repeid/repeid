70fd96a1d706bd1ae5c66e9fc0d8dd656ab66f69
carlos <carlosthe19916@gmail.com>
2016-01-22 11:25:29 -0500

    JPA CONECCION CUSTOM

---
+++ manager/api/jpa/src/main/java/org/repeid/manager/api/jpa/JpaUtils.java
@@ -0,0 +1,20 @@
+/**
+ * @author <a href="mailto:carlosthe19916@sistcoop.com">Carlos Feria</a>
+ */
+package org.repeid.manager.api.jpa;
+
+import javax.persistence.EntityManager;
+
+/**
+ * @author <a href="mailto:mposolda@redhat.com">Marek Posolda</a>
+ */
+public class JpaUtils {
+
+	public static final String HIBERNATE_DEFAULT_SCHEMA = "hibernate.default_schema";
+
+	public static String getTableNameForNativeQuery(String tableName, EntityManager em) {
+		String schema = (String) em.getEntityManagerFactory().getProperties().get(HIBERNATE_DEFAULT_SCHEMA);
+		return (schema == null) ? tableName : schema + "." + tableName;
+	}
+	
+}
