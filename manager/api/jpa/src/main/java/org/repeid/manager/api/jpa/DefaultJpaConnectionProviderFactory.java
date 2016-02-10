503cd79cac4799df982522bfc6ebaf341f413d1f
carlos <carlosthe19916@gmail.com>
2016-02-10 12:00:40 -0500

    jpa name

--- manager/api/jpa/src/main/java/org/repeid/manager/api/jpa/DefaultJpaConnectionProviderFactory.java
+++ manager/api/jpa/src/main/java/org/repeid/manager/api/jpa/DefaultJpaConnectionProviderFactory.java
@@ -69,7 +69,7 @@ public class DefaultJpaConnectionProviderFactory implements JpaConnectionProvide
 
 			Map<String, Object> properties = new HashMap<String, Object>();
 
-			String unitName = "repeid-defaultJpa";
+			String unitName = "repeid-default";
 
 			String dataSource = config.get("dataSource");
 			if (dataSource != null) {
