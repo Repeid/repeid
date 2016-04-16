70234febc184cfe6aa88d2321428e06a2fefd565
carlosthe19916 <carlosthe19916@gmail.com>
2016-04-15 23:16:10 -0500

    1

--- manager/api/services/src/main/java/org/repeid/services/resources/RepeidApplication.java
+++ manager/api/services/src/main/java/org/repeid/services/resources/RepeidApplication.java
@@ -1,6 +1,9 @@
 package org.repeid.services.resources;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.net.URL;
 import java.nio.file.FileSystem;
@@ -9,6 +12,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Properties;
 import java.util.Set;
 
@@ -21,8 +25,12 @@ import org.jboss.resteasy.core.Dispatcher;
 import org.jboss.resteasy.spi.ResteasyProviderFactory;
 import org.keycloak.Config;
 import org.keycloak.common.util.SystemEnvProperties;
+import org.keycloak.util.JsonSerialization;
+import org.repeid.models.ModelDuplicateException;
+import org.repeid.models.RepeidSession;
 import org.repeid.models.RepeidSessionFactory;
 import org.repeid.services.DefaultRepeidSessionFactory;
+import org.repeid.services.ServicesLogger;
 import org.repeid.services.filters.RepeidTransactionCommitter;
 import org.repeid.services.resources.admin.impl.AdminRootImpl;
 import org.repeid.services.resources.impl.JsResourceImpl;
@@ -30,11 +38,14 @@ import org.repeid.services.resources.impl.RobotsResourceImpl;
 import org.repeid.services.resources.impl.ServerVersionResourceImpl;
 import org.repeid.services.resources.impl.ThemeResourceImpl;
 import org.repeid.services.util.JsonConfigProvider;
+import org.repeid.services.util.ObjectMapperResolver;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class RepeidApplication extends Application {
+
+    private static final ServicesLogger logger = ServicesLogger.ROOT_LOGGER;
 
     protected Set<Object> singletons = new HashSet<>();
     protected Set<Class<?>> classes = new HashSet<>();
@@ -49,17 +60,87 @@ public class RepeidApplication extends Application {
         this.sessionFactory = createSessionFactory();
 
         dispatcher.getDefaultContextObjects().put(RepeidApplication.class, this);
-        ResteasyProviderFactory.pushContext(RepeidApplication.class, this); // for injection
+        ResteasyProviderFactory.pushContext(RepeidApplication.class, this); // for
+                                                                            // injection
         context.setAttribute(RepeidSessionFactory.class.getName(), this.sessionFactory);
 
         singletons.add(new ServerVersionResourceImpl());
         singletons.add(new RobotsResourceImpl());
-        //singletons.add(new RealmsResource()); for public access
+        // singletons.add(new RealmsResource()); for public access
         singletons.add(new AdminRootImpl());
         classes.add(ThemeResourceImpl.class);
         classes.add(JsResourceImpl.class);
 
         classes.add(RepeidTransactionCommitter.class);
+
+        singletons.add(new ObjectMapperResolver(
+                Boolean.parseBoolean(System.getProperty("repeid.jsonPrettyPrint", "false"))));
+
+        ExportImportManager exportImportManager;
+
+        DBLockManager dbLockManager = new DBLockManager(sessionFactory.create());
+        dbLockManager.checkForcedUnlock();
+        DBLockProvider dbLock = dbLockManager.getDBLock();
+        dbLock.waitForLock();
+        try {
+            migrateModel();
+
+            KeycloakSession session = sessionFactory.create();
+            try {
+                session.getTransaction().begin();
+
+                ApplianceBootstrap applianceBootstrap = new ApplianceBootstrap(session);
+                exportImportManager = new ExportImportManager(session);
+
+                boolean createMasterRealm = applianceBootstrap.isNewInstall();
+                if (exportImportManager.isRunImport() && exportImportManager.isImportMasterIncluded()) {
+                    createMasterRealm = false;
+                }
+
+                if (createMasterRealm) {
+                    applianceBootstrap.createMasterRealm(contextPath);
+                }
+                session.getTransaction().commit();
+            } catch (RuntimeException re) {
+                if (session.getTransaction().isActive()) {
+                    session.getTransaction().rollback();
+                }
+                throw re;
+            } finally {
+                session.close();
+            }
+
+            if (exportImportManager.isRunImport()) {
+                exportImportManager.runImport();
+            } else {
+                importRealms();
+            }
+
+            importAddUser();
+        } finally {
+            dbLock.releaseLock();
+        }
+
+        if (exportImportManager.isRunExport()) {
+            exportImportManager.runExport();
+        }
+
+        boolean bootstrapAdminUser = false;
+        RepeidSession session = sessionFactory.create();
+        try {
+            session.getTransaction().begin();
+            bootstrapAdminUser = new ApplianceBootstrap(session).isNoMasterUser();
+
+            session.getTransaction().commit();
+        } finally {
+            session.close();
+        }
+
+        // sessionFactory.publish(new PostMigrationEvent());
+
+        singletons.add(new WelcomeResource(bootstrapAdminUser));
+
+        setupScheduledTasks(sessionFactory);
     }
 
     @Override
@@ -78,6 +159,21 @@ public class RepeidApplication extends Application {
         return factory;
     }
 
+    protected void migrateModel() {
+        KeycloakSession session = sessionFactory.create();
+        try {
+            session.getTransaction().begin();
+            MigrationModelManager.migrate(session);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+            logger.migrationFailure(e);
+            throw e;
+        } finally {
+            session.close();
+        }
+    }
+
     public String getContextPath() {
         return contextPath;
     }
@@ -91,24 +187,25 @@ public class RepeidApplication extends Application {
     public URI getBaseUri(UriInfo uriInfo) {
         return uriInfo.getBaseUriBuilder().replacePath(getContextPath()).build();
     }
-    
+
     public static void loadConfig() {
         try {
             JsonNode node = null;
 
             String configDir = System.getProperty("jboss.server.config.dir");
             if (configDir != null) {
-                FileSystem fs = FileSystems.getDefault();
-                Path path = Paths.get(configDir + fs.getSeparator() + "repeid-server.json");
-                if (Files.exists(path) && Files.isRegularFile(path)) {
-                    node = new ObjectMapper().readTree(Files.newInputStream(path));
+                File f = new File(configDir + File.separator + "keycloak-server.json");
+                if (f.isFile()) {
+                    logger.loadingFrom(f.getAbsolutePath());
+                    node = new ObjectMapper().readTree(f);
                 }
             }
 
             if (node == null) {
-                String s = "META-INF/repeid-server.json";
-                URL resource = Thread.currentThread().getContextClassLoader().getResource(s);
+                URL resource = Thread.currentThread().getContextClassLoader()
+                        .getResource("META-INF/keycloak-server.json");
                 if (resource != null) {
+                    logger.loadingFrom(resource);
                     node = new ObjectMapper().readTree(resource);
                 }
             }
@@ -118,10 +215,161 @@ public class RepeidApplication extends Application {
                 Config.init(new JsonConfigProvider(node, properties));
                 return;
             } else {
-                throw new RuntimeException("Config 'repeid-server.json' not found");
+                throw new RuntimeException("Config 'keycloak-server.json' not found");
             }
         } catch (IOException e) {
             throw new RuntimeException("Failed to load config", e);
+        }
+    }
+
+    public static KeycloakSessionFactory createSessionFactory() {
+        DefaultKeycloakSessionFactory factory = new DefaultKeycloakSessionFactory();
+        factory.init();
+        return factory;
+    }
+
+    public static void setupScheduledTasks(final KeycloakSessionFactory sessionFactory) {
+        long interval = Config.scope("scheduled").getLong("interval", 60L) * 1000;
+
+        KeycloakSession session = sessionFactory.create();
+        try {
+            TimerProvider timer = session.getProvider(TimerProvider.class);
+            timer.schedule(
+                    new ClusterAwareScheduledTaskRunner(sessionFactory, new ClearExpiredEvents(), interval),
+                    interval, "ClearExpiredEvents");
+            timer.schedule(new ClusterAwareScheduledTaskRunner(sessionFactory, new ClearExpiredUserSessions(),
+                    interval), interval, "ClearExpiredUserSessions");
+            new UsersSyncManager().bootstrapPeriodic(sessionFactory, timer);
+        } finally {
+            session.close();
+        }
+    }
+
+    public KeycloakSessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        return classes;
+    }
+
+    @Override
+    public Set<Object> getSingletons() {
+        return singletons;
+    }
+
+    public void importRealms() {
+        String files = System.getProperty("keycloak.import");
+        if (files != null) {
+            StringTokenizer tokenizer = new StringTokenizer(files, ",");
+            while (tokenizer.hasMoreTokens()) {
+                String file = tokenizer.nextToken().trim();
+                RealmRepresentation rep;
+                try {
+                    rep = loadJson(new FileInputStream(file), RealmRepresentation.class);
+                } catch (FileNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+                importRealm(rep, "file " + file);
+            }
+        }
+    }
+
+    public void importRealm(RealmRepresentation rep, String from) {
+        KeycloakSession session = sessionFactory.create();
+        boolean exists = false;
+        try {
+            session.getTransaction().begin();
+
+            try {
+                RealmManager manager = new RealmManager(session);
+                manager.setContextPath(getContextPath());
+
+                if (rep.getId() != null && manager.getRealm(rep.getId()) != null) {
+                    logger.realmExists(rep.getRealm(), from);
+                    exists = true;
+                }
+
+                if (manager.getRealmByName(rep.getRealm()) != null) {
+                    logger.realmExists(rep.getRealm(), from);
+                    exists = true;
+                }
+                if (!exists) {
+                    RealmModel realm = manager.importRealm(rep);
+                    logger.importedRealm(realm.getName(), from);
+                }
+                session.getTransaction().commit();
+            } catch (Throwable t) {
+                session.getTransaction().rollback();
+                if (!exists) {
+                    logger.unableToImportRealm(t, rep.getRealm(), from);
+                }
+            }
+        } finally {
+            session.close();
+        }
+    }
+
+    public void importAddUser() {
+        String configDir = System.getProperty("jboss.server.config.dir");
+        if (configDir != null) {
+            File addUserFile = new File(configDir + File.separator + "keycloak-add-user.json");
+            if (addUserFile.isFile()) {
+                logger.imprtingUsersFrom(addUserFile);
+
+                List<RealmRepresentation> realms;
+                try {
+                    realms = JsonSerialization.readValue(new FileInputStream(addUserFile),
+                            new TypeReference<List<RealmRepresentation>>() {
+                            });
+                } catch (IOException e) {
+                    logger.failedToLoadUsers(e);
+                    return;
+                }
+
+                for (RealmRepresentation realmRep : realms) {
+                    for (UserRepresentation userRep : realmRep.getUsers()) {
+                        KeycloakSession session = sessionFactory.create();
+                        try {
+                            session.getTransaction().begin();
+
+                            RealmModel realm = session.realms().getRealmByName(realmRep.getRealm());
+                            if (realm == null) {
+                                logger.addUserFailedRealmNotFound(userRep.getUsername(), realmRep.getRealm());
+                            } else {
+                                UserModel user = session.users().addUser(realm, userRep.getUsername());
+                                user.setEnabled(userRep.isEnabled());
+                                RepresentationToModel.createCredentials(userRep, user);
+                                RepresentationToModel.createRoleMappings(userRep, user, realm);
+                            }
+
+                            session.getTransaction().commit();
+                            logger.addUserSuccess(userRep.getUsername(), realmRep.getRealm());
+                        } catch (ModelDuplicateException e) {
+                            session.getTransaction().rollback();
+                            logger.addUserFailedUserExists(userRep.getUsername(), realmRep.getRealm());
+                        } catch (Throwable t) {
+                            session.getTransaction().rollback();
+                            logger.addUserFailed(t, userRep.getUsername(), realmRep.getRealm());
+                        } finally {
+                            session.close();
+                        }
+                    }
+                }
+
+                if (!addUserFile.delete()) {
+                    logger.failedToDeleteFile(addUserFile.getAbsolutePath());
+                }
+            }
+        }
+    }
+
+    private static <T> T loadJson(InputStream is, Class<T> type) {
+        try {
+            return JsonSerialization.readValue(is, type);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to parse json", e);
         }
     }
 
