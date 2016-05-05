2bedab914a0342e6f73110250be30614e0a195fb
carlosthe19916 <carlosthe19916@gmail.com>
2016-05-05 08:48:57 -0500

    initial settings

--- manager/api/services/src/main/java/org/repeid/services/managers/OrganizationManager.java
+++ manager/api/services/src/main/java/org/repeid/services/managers/OrganizationManager.java
@@ -37,108 +37,17 @@ public class OrganizationManager {
     }
 
     public OrganizationModel createOrganization(String name) {
-        return createOrganization(name, name);
+        return createOrganization(null, name);
     }
-    
+
     public OrganizationModel createOrganization(String id, String name) {
-        /*if (id == null) id = KeycloakModelUtils.generateId();
-        RealmModel realm = model.createRealm(id, name);
-        realm.setName(name);
-
-        // setup defaults
-        setupRealmDefaults(realm);
-
-        setupMasterAdminManagement(realm);
-        setupRealmAdminManagement(realm);
-        setupAccountManagement(realm);
-        setupBrokerService(realm);
-        setupAdminConsole(realm);
-        setupAdminConsoleLocaleMapper(realm);
-        setupAdminCli(realm);
-        setupImpersonationService(realm);
-        setupAuthenticationFlows(realm);
-        setupRequiredActions(realm);
-        setupOfflineTokens(realm);
-
-        return realm;*/
-        return null;
+        OrganizationModel realm = model.createOrganization(name);
+        return realm;
     }
 
     public OrganizationModel importOrganization(OrganizationRepresentation rep) {
-        /*String id = rep.getId();
-        if (id == null) {
-            id = KeycloakModelUtils.generateId();
-        }
-        RealmModel realm = model.createRealm(id, rep.getRealm());
-        realm.setName(rep.getRealm());
-
-        // setup defaults
-
-        setupRealmDefaults(realm);
-
-        boolean postponeMasterClientSetup = postponeMasterClientSetup(rep);
-        if (!postponeMasterClientSetup) {
-            setupMasterAdminManagement(realm);
-        }
-
-        if (!hasRealmAdminManagementClient(rep)) setupRealmAdminManagement(realm);
-        if (!hasAccountManagementClient(rep)) setupAccountManagement(realm);
-
-        boolean postponeImpersonationSetup = false;
-        if (hasRealmAdminManagementClient(rep)) {
-            postponeImpersonationSetup = true;
-        } else {
-            setupImpersonationService(realm);
-        }
-
-
-        if (!hasBrokerClient(rep)) setupBrokerService(realm);
-        if (!hasAdminConsoleClient(rep)) setupAdminConsole(realm);
-
-        boolean postponeAdminCliSetup = false;
-        if (!hasAdminCliClient(rep)) {
-            if (hasRealmAdminManagementClient(rep)) {
-                postponeAdminCliSetup = true;
-            } else {
-                setupAdminCli(realm);
-            }
-        }
-
-        if (!hasRealmRole(rep, Constants.OFFLINE_ACCESS_ROLE)) setupOfflineTokens(realm);
-
-        RepresentationToModel.importRealm(session, rep, realm);
-
-        setupAdminConsoleLocaleMapper(realm);
-
-        if (postponeMasterClientSetup) {
-            setupMasterAdminManagement(realm);
-        }
-
-        // Assert all admin roles are available once import took place. This is needed due to import from previous version where JSON file may not contain all admin roles
-        checkMasterAdminManagementRoles(realm);
-        checkRealmAdminManagementRoles(realm);
-
-        // Could happen when migrating from older version and I have exported JSON file, which contains "realm-management" client but not "impersonation" client
-        // I need to postpone impersonation because it needs "realm-management" client and its roles set
-        if (postponeImpersonationSetup) {
-            setupImpersonationService(realm);
-        }
-
-        if (postponeAdminCliSetup) {
-            setupAdminCli(realm);
-        }
-
-        setupAuthenticationFlows(realm);
-        setupRequiredActions(realm);
-
-        // Refresh periodic sync tasks for configured federationProviders
-        List<UserFederationProviderModel> federationProviders = realm.getUserFederationProviders();
-        UsersSyncManager usersSyncManager = new UsersSyncManager();
-        for (final UserFederationProviderModel fedProvider : federationProviders) {
-            usersSyncManager.notifyToRefreshPeriodicSync(session, realm, fedProvider, false);
-        }
-        return realm;*/
-        return null;
+        OrganizationModel organization = model.createOrganization(null, rep.getName());
+        return organization;
     }
 
 }
