ae2b97c3860c7c88aa2e417f2d99fb90d0ad8bd1
carlosthe19916 <carlosthe19916@gmail.com>
2016-06-15 11:09:44 -0500

    set correct format

--- server-spi/src/main/java/org/repeid/cluster/ClusterEvent.java
+++ server-spi/src/main/java/org/repeid/cluster/ClusterEvent.java
@@ -2,8 +2,5 @@ package org.repeid.cluster;
 
 import java.io.Serializable;
 
-/**
- * @author <a href="mailto:mposolda@redhat.com">Marek Posolda</a>
- */
 public interface ClusterEvent extends Serializable {
 }
--- server-spi/src/main/java/org/repeid/cluster/ClusterListener.java
+++ server-spi/src/main/java/org/repeid/cluster/ClusterListener.java
@@ -2,16 +2,16 @@ package org.repeid.cluster;
 
 /**
  * Task to be executed on all cluster nodes once it's notified.
- *
- * @author <a href="mailto:mposolda@redhat.com">Marek Posolda</a>
  */
 public interface ClusterListener {
 
-    /**
-     * Registered task to be executed on all cluster nodes once it's notified from cache.
-     *
-     * @param event value of notification (Object added into the cache)
-     */
-    void run(ClusterEvent event);
+	/**
+	 * Registered task to be executed on all cluster nodes once it's notified
+	 * from cache.
+	 *
+	 * @param event
+	 *            value of notification (Object added into the cache)
+	 */
+	void run(ClusterEvent event);
 
 }
